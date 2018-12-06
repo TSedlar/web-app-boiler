98979723cab22ba6a03e5d07942512c186dd4ea0
Tyler Sedlar <sedlarizona@gmail.com>
2018-12-06 03:21:28 -0700

    Add service worker

--- src/index.js
+++ src/index.js
@@ -6,17 +6,11 @@ import * as serviceWorker from "./serviceWorker";
 
 const startApp = () => {
   ReactDOM.render(<App />, document.getElementById("root"));
+  serviceWorker.register();
 };
 
-startApp();
-
-// if (!window.cordova) {
-//   startApp();
-// } else {
-//   document.addEventListener("deviceready", startApp, false);
-// }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (!window.cordova) {
+  startApp();
+} else {
+  document.addEventListener("deviceready", startApp, false);
+}
